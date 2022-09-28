65daca321501a0bbb9bbdd2257f5f4aa5cc6213a
Adegoroye Oludolapo <doltil@yahoo.co.uk>
2022-09-28 22:55:19 +0100

    added forecast section

--- src/app.js
+++ src/app.js
@@ -1,21 +1,113 @@
 function getCelsiusTemperature() {
-  const celsius = document.querySelector('#temperature');
-  const celsiusLink = document.querySelector('#celsius-link');
-  const fahrenheitLink = document.querySelector('#fahrenheit-link');
-  celsius.innerHTML = Math.round(celsiusTemp);
+  isCelsius = true;
+  const searchInput = document.querySelector('#search');
+  if (searchInput.value === '') {
+    getCurrentLocation();
+  } else {
+    searchTemperature(city);
+  }
   celsiusLink.classList.add('active');
   fahrenheitLink.classList.remove('active');
 }
 function getFahrenheitTemperature() {
-  const celsius = document.querySelector('#temperature');
-  const celsiusLink = document.querySelector('#celsius-link');
-  const fahrenheitLink = document.querySelector('#fahrenheit-link');
-  const fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
-  celsius.innerHTML = Math.round(fahrenheitTemp);
+  isCelsius = false;
+  const searchInput = document.querySelector('#search');
+  if (searchInput.value === '') {
+    getCurrentLocation();
+  } else {
+    searchTemperature(city);
+  }
   celsiusLink.classList.remove('active');
   fahrenheitLink.classList.add('active');
 }
+function formatDay(time) {
+  const date = new Date(time * 1000);
+  const day = date.getDay();
+  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  return daysOfWeek[day];
+}
+function getForecastDetails(daily) {
+  const forecast = daily;
+  let renderForecast = '';
+  let weatherForecastElement = document.querySelector('#weather-forecast');
+  forecast.forEach(function (dayOfForecast, index) {
+    if (index < 5) {
+      renderForecast =
+        renderForecast +
+        `<div class="col-md col-sm-4 py-2">
+              <div class="card next-day">
+                <div class="card-body">
+                  <h3>${formatDay(dayOfForecast.dt)}</h3>
+                  <img src="http://openweathermap.org/img/wn/${
+                    dayOfForecast.weather[0].icon
+                  }@2x.png" alt="Clear" class="icon" />
+                  <p><span id="lower-temp">${Math.round(
+                    dayOfForecast.temp.min
+                  )}°${
+          isCelsius ? 'C' : 'F'
+        }</span>| <span id="higher-temp">${Math.round(
+          dayOfForecast.temp.max
+        )}°${isCelsius ? 'C' : 'F'}</span></p>
+                </div>
+              </div>
+            </div>`;
+    }
+  });
+  weatherForecastElement.innerHTML = renderForecast;
+}
 function renderData(weatherDetails) {
+  const cityElement = document.querySelector('#city');
+  const countryElement = document.querySelector('#country');
+  const celsius = document.querySelector('#temperature');
+  const pressure = document.querySelector('#pressure');
+  const humidity = document.querySelector('#humidity');
+  const wind = document.querySelector('#wind');
+  const weatherDesc = document.querySelector('#weather-desc');
+  const iconCurrent = document.querySelector('#icon-current');
+  const dayElement = document.querySelector('#day');
+  const timeElement = document.querySelector('#time');
+  cityElement.innerHTML = weatherDetails.city;
+  countryElement.innerHTML = weatherDetails.country;
+  celsius.innerHTML = weatherDetails.temp;
+  pressure.innerHTML = `${weatherDetails.pressure}hPa`;
+  humidity.innerHTML = `${weatherDetails.humidity}%`;
+  wind.innerHTML = isCelsius
+    ? `${weatherDetails.wind}metre/sec`
+    : `${weatherDetails.wind}miles/hour`;
+  weatherDesc.innerHTML = weatherDetails.weatherDesc;
+  iconCurrent.setAttribute(
+    'src',
+    `http://openweathermap.org/img/wn/${weatherDetails.weatherIcon}@2x.png`
+  );
+  iconCurrent.setAttribute('alt', weatherDetails.weatherDesc);
+  dayElement.innerHTML = weatherDetails.day;
+  timeElement.innerHTML = `${
+    weatherDetails.hours < 10
+      ? `0${weatherDetails.hours}`
+      : weatherDetails.hours
+  }:${
+    weatherDetails.minutes < 10
+      ? `0${weatherDetails.minutes}`
+      : weatherDetails.minutes
+  }`;
+  getForecastDetails(weatherDetails.daily);
+}
+function getCityCoord(response) {
+  const lon = response.data.coord.lon;
+  const lat = response.data.coord.lat;
+  searchCurrentTemperature(lon, lat);
+}
+function getCityInfo(response) {
+  city = response.data.name;
+  country = response.data.sys.country;
+
+  const countryElement = document.querySelector('#country');
+  const cityElement = document.querySelector('#city');
+
+  cityElement.innerHTML = city;
+  countryElement.innerHTML = country;
+}
+function getLocationInfo(response) {
   const days = [
     'Sunday',
     'Monday',
@@ -25,74 +117,48 @@ function renderData(weatherDetails) {
     'Friday',
     'Saturday',
   ];
-  const date = new Date(weatherDetails.date * 1000);
+  const date = new Date(response.data.current.dt * 1000);
   const day = days[date.getDay()];
   const hours = date.getHours();
   const minutes = date.getMinutes();
-  const cityElement = document.querySelector('#city');
-  const dayElement = document.querySelector('#day');
-  const timeElement = document.querySelector('#time');
-  const countryElement = document.querySelector('#country');
-  const celsius = document.querySelector('#temperature');
-  const pressure = document.querySelector('#pressure');
-  const humidity = document.querySelector('#humidity');
-  const wind = document.querySelector('#wind');
-  const weatherDesc = document.querySelector('#weather-desc');
-  const iconCurrent = document.querySelector('#icon-current');
-  cityElement.innerHTML = weatherDetails.city;
-  countryElement.innerHTML = weatherDetails.country;
-  dayElement.innerHTML = day;
-  timeElement.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  }`;
-  celsius.innerHTML = weatherDetails.temp;
-  pressure.innerHTML = `${weatherDetails.pressure}hPa`;
-  humidity.innerHTML = `${weatherDetails.humidity}%`;
-  wind.innerHTML = `${weatherDetails.wind}km/h`;
-  weatherDesc.innerHTML = weatherDetails.weatherDesc;
-  iconCurrent.setAttribute(
-    'src',
-    `http://openweathermap.org/img/wn/${weatherDetails.weatherIcon}@2x.png`
-  );
-  iconCurrent.setAttribute('alt', weatherDetails.weatherDesc);
-}
-
-function getLocationInfo(response) {
-  const city = response.data.name;
-  const pressure = response.data.main.pressure;
-  const humidity = response.data.main.humidity;
-  const weatherDesc = response.data.weather[0].description;
-  const weatherIcon = response.data.weather[0].icon;
-  const wind = Math.round(response.data.wind.speed);
-  celsiusTemp = response.data.main.temp;
+  const pressure = response.data.current.pressure;
+  const humidity = response.data.current.humidity;
+  const weatherDesc = response.data.current.weather[0].description;
+  const weatherIcon = response.data.current.weather[0].icon;
+  const wind = Math.round(response.data.current.wind_speed);
+  const daily = response.data.daily;
+  celsiusTemp = response.data.current.temp;
   const temp = Math.round(celsiusTemp);
-  const date = response.data.dt;
-  const country = response.data.sys.country;
   const weatherDetails = {
     city,
+    country,
     pressure,
     humidity,
     weatherDesc,
     weatherIcon,
     wind,
     temp,
-    date,
-    country,
+    day,
+    hours,
+    minutes,
+    daily,
   };
   renderData(weatherDetails);
 }
 function searchCurrentTemperature(longitude, latitude) {
   const apiKey = config.k;
-  const unit = 'metric';
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`;
+  const unit = isCelsius ? 'metric' : 'imperial';
+  const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`;
+  const apiUrl2 = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`;
   axios.get(apiUrl).then(getLocationInfo);
+  axios.get(apiUrl2).then(getCityInfo);
 }
 function searchTemperature(city) {
   const apiKey = 'cf25d8eb42806c8d7039bbac5d23b349';
-  const unit = 'metric';
+  const unit = isCelsius ? 'metric' : 'imperial';
   if (city !== '') {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
-    axios.get(apiUrl).then(getLocationInfo);
+    axios.get(apiUrl).then(getCityCoord);
   }
 }
 
@@ -111,6 +177,9 @@ function handleSearch(e) {
 }
 
 let celsiusTemp = null;
+let isCelsius = true;
+let city = '';
+let country = '';
 const searchForm = document.querySelector('#search-form');
 const celsiusLink = document.querySelector('#celsius-link');
 const fahrenheitLink = document.querySelector('#fahrenheit-link');
